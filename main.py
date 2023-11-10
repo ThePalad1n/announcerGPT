671675874783b868aa510ad88aaf21d0ef5d141f
ThePalad1n <evangreen91@gmail.com>
2023-11-09 23:34:03 -0500

    speech api connected

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os
+from dotenv import load_dotenv
 import cv2
 import numpy as np
 import pyautogui
 import time
+from pathlib import Path
+from openai import OpenAI
+client = OpenAI(
+        # Get the API key from the environment variable
+    api_key = os.getenv('API_KEY')
+)
+
 
 # Set the interval between captures (in seconds).
 capture_interval = 10  # Capture every 10 seconds
@@ -10,17 +19,40 @@ capture_interval = 10  # Capture every 10 seconds
 while True:
     # Capture the screen
     screenshot = pyautogui.screenshot()
-    
+
     # Convert the screenshot to a format that OpenCV can read
     frame = np.array(screenshot)
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Convert the image color to BGR
-    
+    # Convert the image color to BGR
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
     # Save the captured image to disk
-    timestamp = time.strftime("%Y%m%d-%H%M%S")  # Get a timestamp for the filename
-    filename = f"screenshot_{timestamp}.png"  # Create a filename with the timestamp
+    # Get a timestamp for the filename
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    # Create a filename with the timestamp
+    filename = f"screenshot_{timestamp}.png"
     cv2.imwrite(filename, frame)  # Save the image
-    
+
     print(f"Captured {filename}")
 
     # Wait for the specified interval before capturing the next screenshot
     time.sleep(capture_interval)
+
+
+    # Load the environment variables from the .env file
+    load_dotenv()
+
+
+
+    # Output file path for the MP3
+    speech_file_path = "speech.mp3"
+
+    # Call the OpenAI API to generate speech
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="echo",
+        input="Today is a wonderful day to build something people love!"
+    )
+
+    # Save the generated speech to an MP3 file
+    with open(speech_file_path, 'wb') as f:
+        f.write(response.content)
